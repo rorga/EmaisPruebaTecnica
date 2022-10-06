884de463f81af9468fb00dd8345ecc53f3698bc0
rorga <raul_orga@hotmail.com>
2022-10-06 23:40:52 +0200

    #Configuración inicial de rutas, estilos y página principal FIX

--- EmaisPruebaTecnica/src/app/app.module.ts
+++ EmaisPruebaTecnica/src/app/app.module.ts
@@ -3,7 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { MainPageComponent } from './pages/main-page/main-page.component';
+import { MainPageComponent } from './components/main-page/main-page.component';
 
 @NgModule({
   declarations: [
